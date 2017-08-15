8aa72f5bbb29564fae88a8a1935999c96ef6a2dc
mohamed amin <mohamedamine.mhiri@esprit.tn>
2017-08-15 17:38:09 +0100

    second change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 var server = require('http').createServer(app); 
 const io = require('socket.io').listen(4000);
@@ -8,20 +9,73 @@ const Baby= require('babyparse');
 const bodyParser = require('body-parser');
 let upload = multer({dest: 'uploads/'})
 const fs = require('fs')
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/csvfiles');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport')
+var mongoose = require('mongoose');
+const config = require('./config/database')
+mongoose.connect(config.database);
 const db= mongoose.connection;
+let User = require('./model/user')
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}));
+//pasport config
+  app.use(passport.initialize());
+  app.use(passport.session());
+require('./config/passport')(passport);
+app.get('*',function(req,res,next){
+  res.locals.user = req.user || null
+  next()
+})
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 let csv = require('./routes/csv');
+let users = require('./routes/users');
 app.use('/csv',csv)
+app.use('/users',users)
 //index
-app.get('/',function (req,res) {
-  res.render('index');
+app.get('/', ensureAuthentification, function (req,res) {
+
+  var userId= req.user._id
+     User.findById(userId,function(err,user){
+        res.render('index',{
+          name : user.name
+        })
+          })
 });
 
 
@@ -32,6 +86,10 @@ app.use(function (req,res,next) {
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
   next();
 })
+
+
+
+
 
 
 
@@ -57,8 +115,15 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
-
-server.listen(3000,function () {
+function ensureAuthentification(req, res, next){
+if (req.isAuthenticated()) {
+  return next();
+}else{
+  req.flash('please login')
+  res.redirect('/users/login')
+}
+}
+server.listen(process.env.PORT || 8080,function () {
 	console.log('welcome');
 	// body...
 });
--- routes/csv.js
+++ routes/csv.js
@@ -14,6 +14,9 @@ const db= mongoose.connection;
 var entete = []
 var variable
 var filename;
+let User = require('../model/user')
+let CsvM = require('../model/csvfiles')
+
 //function parse
 function parcih(req,res) {
 	var file =fs.readFileSync(req.file.path)+""
@@ -41,7 +44,9 @@ Baby.parse(file, {
 
 
 
-router.post('/', upload.single('myfile'), function (req,res) {
+router.post('/',ensureAuthentification , upload.single('myfile'), function (req,res) {
+  console.log(req.user._id)
+  
 parcih(req,res);
 var key = [];//nombre de champs
 var keys = Object.keys(donne[1]);
@@ -58,11 +63,10 @@ res.send({donne,entete})
 
 
 
-router.post('/add',function (req,res) {
+router.post('/add', ensureAuthentification, function (req,res) {
  //console.log(variable)
  //console.log(req.body)
   var newentete = []
-
   var numberentete = Object.keys(req.body).length;
   console.log(numberentete)
   //console.log(Object.keys(req.body).length);
@@ -73,67 +77,87 @@ router.post('/add',function (req,res) {
   console.log(entete)
   //-------------------------------------------------------------------------------------------------
   //comparison
-
-
   //get header donne
 //update header
-
 for (var j = 0; j < variable.length; j++) {
   for (var i = 0; i < entete.length; i++) {
-
- 
 if (entete[i] !== newentete[i] && newentete[i] !=="") {
   variable[j][newentete[i]] = variable[j][entete[i]]
-
 delete variable[j][entete[i]];
-
 }
-
 }
-
 }
 console.log("el filter sayer")
-
 //end update header
-   
  // to delete the skip files
 for (var j = 0; j < variable.length; j++) {
   for (var i = 0; i < entete.length; i++) {
 
     if (newentete[i] ==="") {
     delete variable[j][entete[i]];
-
   }
 }
-
 }
 console.log("el tafsi5 sayer")
-
+// add id user connected
+var newUser = 'user_id'
+var newValue = req.user._id
+for (var j = 0; j < variable.length; j++) {
+  variable[j][newUser] = newValue ;
+}
+//req.user._id;
 console.log(variable)
 //end delete skipped files
-
-
-// create the final file
-//donne = results.data
- 
 for (var i = 0; i < variable.length; i++) {
   //console.log("part("+i+")")
   db.collection(filename).save(variable[i], function(err, records) {
         if (err) throw err;
       //  console.log("record added");
-
       });
-
 }
-
-console.log("el base sayer")
-
 // mongodb
-
-
 variable = new Object();
 entete = [];
-            res.sendStatus(200);
+ res.sendStatus(200);
 });
 
+
+router.get('/show', ensureAuthentification, function (req,res) {
+  var userId= req.user._id
+  console.log(userId)
+     User.findById(userId,function(err,user){
+      if (err) {throw err}else {
+            db.collection('normal.csv').find({ user_id: userId }).toArray( function(err, csvm){
+         res.render('showcsv',{
+          name : user.name,
+          csvm : csvm
+        })
+    })
+      }
+ 
+          })
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+function ensureAuthentification(req, res, next){
+if (req.isAuthenticated()) {
+  return next();
+}else{
+  req.flash('please login')
+  res.redirect('/users/login')
+}
+}
 module.exports = router;
